50ab7e02a92d59a319c5570020f915d780fb46db
unknown <apridika14@gmail.com>
2026-05-05 15:53:25 +0700

    add animasi card

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// fungsi untuk menambahkan class active pada link yang sesuai dengan halaman saat ini
 const links = document.querySelectorAll(".nav-links a");
 
 // ambil nama file aktif
@@ -8,10 +9,57 @@ if (currentPage === "") {
   currentPage = "index.html";
 }
 
-links.forEach(link => {
+links.forEach((link) => {
   let linkPage = link.getAttribute("href");
 
   if (linkPage === currentPage) {
     link.classList.add("active");
   }
+});
+
+
+// fungsi scroll ke layanan
+function scrollLayanan() {
+  document.getElementById("layanan").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// fungsi scroll ke layanan dengan animasi munculnya card
+const cards = document.querySelectorAll(".service-card");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      
+      // animasi delay biar muncul satu-satu
+      setTimeout(() => {
+        entry.target.classList.add("show");
+      }, index * 300);
+
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+cards.forEach(card => {
+  observer.observe(card);
+});
+
+const galleryCards = document.querySelectorAll(".gallery-card");
+
+const galleryObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+galleryCards.forEach((card, index) => {
+  card.style.transitionDelay = `${index * 0.25}s`;
+  galleryObserver.observe(card);
 });
